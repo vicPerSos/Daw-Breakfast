c3d61cb6596d5daf0c2017ea410bbe391bd39eef
vicPerSos <vpsb888@gmail.com>
2025-02-12 20:50:48 +0100

    Subida de implemetacion de desayuno hasta controller y correcion en usuario

--- src/main/java/com/breakfast/daw/persintence/repositories/DesayunoRepository.java
+++ src/main/java/com/breakfast/daw/persintence/repositories/DesayunoRepository.java
@@ -1,11 +1,17 @@
 package com.breakfast.daw.persintence.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.breakfast.daw.persintence.entities.Desayuno;
+import com.breakfast.daw.persintence.entities.Establecimiento;
 
-@Repository
 public interface DesayunoRepository extends JpaRepository<Desayuno, Integer> {
+	List<Desayuno> findAllByOrderByPuntuacionDesc();
 
+	List<Desayuno> findByEstablecimientoOrderByPuntuacionDesc(Establecimiento establecimiento);
+
+	List<Desayuno> findByEstablecimientoOrderByPrecioAsc(Establecimiento establecimiento);
+
+	List<Desayuno> findByEstablecimiento(Establecimiento establecimiento);
 }
